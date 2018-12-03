cf9d85500bc09467e8dfed12646047d6a94257c4
Tech-A42 <tech-a42@tech-a42s-air.home>
2018-12-03 20:50:40 +0000

    second color added

--- main.js
+++ main.js
@@ -2,45 +2,61 @@ const squares = document.getElementsByTagName("td")
 let info = document.getElementById('info');
 let game = false;
 let clicks = [];
+let red = false;
+let green = false;
 
 
-// got to try and make if square[0] is clicked game starts with the color red
 
 
 
+// Start points 
 squares[0].addEventListener("click", () => {
   gameStart();
-  console.log("STARTED")
+  console.log("Red started")
+  red = true;
 });
 
+squares[3].addEventListener("click", () => {
+  gameStart();
+  console.log("Green started")
+  green = true;
 
+})
 
-
-function wrapper() {
+function settingBoard() {
   for (let i = 0; i < squares.length; i++) {
-    if (game === true) {
+    if (game == true) {
       squares[i].getAttribute("data-num")
       squares[i].setAttribute("class", "");
       squares[i].addEventListener("click", () => {
         onSquareClick(i)
-
       });
     }
   }
 }
 
-
-
+//Chooses colour of square click
 function onSquareClick(i) {
-  squares[i].setAttribute("class", "0");
-  squares[i].style.backgroundColor = "red";
-}
 
+  if (green == true) {
+    red = false;
+    squares[i].setAttribute("class", "X");
+    squares[i].style.backgroundColor = "green";
+  }
+
+  if (red == true) {
+    green = false;
+    squares[i].setAttribute("class", "0");
+    squares[i].style.backgroundColor = "red";
+  }
+
+}
 function gameStart() {
   console.log("gamestarting");
   game = true;
-  wrapper()
+  settingBoard()
 }
+
 // Creating function that will tell the position of cursor
 // PageX and PageY will getting position values and show them in P
 function tellPos(p) {
