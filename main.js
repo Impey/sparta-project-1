aed9a2b3231d50c3cc3cddf4e952f27dfe917d31
Tech-A42 <tech-a42@Tech-A42s-MacBook-Air.local>
2018-12-03 16:53:48 +0000

    Added colour selector

--- main.js
+++ main.js
@@ -1,32 +1,46 @@
 const squares = document.getElementsByTagName("td")
 let info = document.getElementById('info');
-
-
-
 let game = false;
+let clicks = [];
 
 
 // got to try and make if square[0] is clicked game starts with the color red
 
 
-if (game == true) {
+
+squares[0].addEventListener("click", () => {
+  gameStart();
+  console.log("STARTED")
+});
+
+
+
+
+function wrapper() {
   for (let i = 0; i < squares.length; i++) {
-    squares[i].getAttribute("data-num")
-    squares[i].setAttribute("class", "");
-    squares[i].addEventListener("click", () => {
-      onSquareClick(i)
-      clicked = console.log(i);
-    });
+    if (game === true) {
+      squares[i].getAttribute("data-num")
+      squares[i].setAttribute("class", "");
+      squares[i].addEventListener("click", () => {
+        onSquareClick(i)
+
+      });
+    }
   }
 }
+
+
+
 function onSquareClick(i) {
   squares[i].setAttribute("class", "0");
   squares[i].style.backgroundColor = "red";
-
 }
 
-
-
+function gameStartRed() {
+  console.log("gamestarting");
+  game = true;
+  wrapper()
+}
 // Creating function that will tell the position of cursor
 // PageX and PageY will getting position values and show them in P
 function tellPos(p) {
