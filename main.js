47ce86d3bc57a1eda4869b365b675f0c3d3010ba
Tech-A42 <tech-a42@Tech-A42s-MacBook-Air.local>
2018-12-03 14:49:24 +0000

    Mouse position function added

--- main.js
+++ main.js
@@ -1,25 +1,41 @@
 const squares = document.getElementsByTagName("td")
-
-
-for (let i = 0; i < squares.length; i++) {
-  squares[i].getAttribute("data-num")
-
-  squares[i].setAttribute("class", "");
-  squares[i].addEventListener("click", () => {
-    console.log("Tile clicked" + i);
-    onSquareClick(i)
-  });
-}
+let info = document.getElementById('info');
 
 
 
+let game = false;
 
-function onSquareClick(i) {
-  if (playersTurn == true) {
-    squares[i].setAttribute("class", "0");
-    squares[i].innerHTML = "0";
+
+// got to try and make if square[0] is clicked game starts with the color red
+
+
+if (game == true) {
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].getAttribute("data-num")
+    squares[i].setAttribute("class", "");
+    squares[i].addEventListener("click", () => {
+      onSquareClick(i)
+      clicked = console.log(i);
+    });
   }
 }
+function onSquareClick(i) {
+  squares[i].setAttribute("class", "0");
+  squares[i].style.backgroundColor = "red";
+
+}
+
+
+
+// Creating function that will tell the position of cursor
+// PageX and PageY will getting position values and show them in P
+function tellPos(p) {
+  info.innerHTML = 'Position X : ' + p.pageX + '<br />Position Y : ' + p.pageY;
+
+}
+addEventListener('mousemove', tellPos, false)
+
+
 
 
 
