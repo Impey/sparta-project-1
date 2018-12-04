8cb11e640c99e9bf1850cac432cf3f3e90ff1b82
Tech-A42 <tech-a42@Tech-A42s-MacBook-Air.local>
2018-12-04 15:07:34 +0000

    adding win COndition

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
 const squares = document.getElementsByTagName("td")
 let info = document.getElementById('info');
+const btnReset = document.getElementById("resetBtn");
+const colSelected = document.getElementById("colSelected")
+let mouseIsDown = false;
 let game = false;
-let clicks = [];
 let red = false;
 let green = false;
-let mouseIsDown = false;
-
-
-
-
-
-
-
-
+let win = false;
 // Start points 
 squares[0].addEventListener("click", () => {
   gameStart();
@@ -20,7 +14,6 @@ squares[0].addEventListener("click", () => {
   red = true;
   green = false;
 });
-
 squares[3].addEventListener("click", () => {
   gameStart();
   console.log("Green started")
@@ -34,43 +27,68 @@ function settingBoard() {
     if (game == true) {
       squares[i].getAttribute("data-num")
       squares[i].setAttribute("class", "");
-
       squares[i].addEventListener('mousedown', function () { mouseIsDown = true })
       squares[i].addEventListener('mouseup', function () { mouseIsDown = false })
       squares[i].addEventListener('mousemove', function () {
         if (mouseIsDown) {
           onSquareClick(i)
           console.log("Hovering")
+
+          if (win == true) {
+            alert("Game is won")
+            resetBoard();
+          }
         }
       })
     }
   }
 }
-
-
-
-
-
-
-//Chooses colour of square click
 function onSquareClick(i) {
-
   if (green == true) {
     red = false;
     squares[i].setAttribute("class", "X");
     squares[i].style.backgroundColor = "green";
+    colSelected.innerHTML = "Green colour selected"
+
   }
   if (red == true) {
     green = false;
     squares[i].setAttribute("class", "0");
     squares[i].style.backgroundColor = "red";
+    colSelected.innerHTML = "Red colour selected"
+
   }
 
+  WinCon();
 }
 function gameStart() {
   console.log("gamestarting");
   game = true;
   settingBoard()
+
+}
+
+
+btnReset.addEventListener("click", () => {
+  resetBoard();
+
+})
+
+function resetBoard() {
+  for (let i = 0; i < squares.length; i++) {
+    squares[parseInt(i)].setAttribute("class", "clear");
+    squares[i].style.backgroundColor = "white";
+    game = false;
+  }
+
+}
+
+function WinCon() {
+  for (let i = 0; i < squares.length; i++) {
+    if (squares[0, 1, 2, 4, 5, 6, 8, 9, 10].className == "0" && squares[3, 7, 11].className == "X") {
+      win = true;
+    }
+  }
 }
 
 // Creating function that will tell the position of cursor
@@ -80,6 +98,12 @@ function tellPos(p) {
 
 }
 addEventListener('mousemove', tellPos, false)
+
+
+
+
+
+
 
 
 
