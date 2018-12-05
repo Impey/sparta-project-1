cf8668591e2441f754876a68d4858c1ace2f903e
Tech-A42 <tech-a42@Tech-A42s-MacBook-Air.local>
2018-12-05 11:14:04 +0000

    Win condition still eludes me

--- main.js
+++ main.js
@@ -7,18 +7,34 @@ let game = false;
 let red = false;
 let green = false;
 let win = false;
+let clicks = []
 const winCon = [
   [0, 1, 2, 4, 5, 6, 8, 9, 10], // red win
   [3, 7, 11]// green win 
 ]
+
 // Start points 
-squares[0].addEventListener("click", () => {
+squares[0].addEventListener("mousedown", () => {
   gameStart();
   console.log("Red started")
   red = true;
   green = false;
 });
+squares[2].addEventListener("click", () => {
+  gameStart();
+  console.log("Red started")
+  red = true;
+  green = false;
+});
+
+
 squares[3].addEventListener("click", () => {
+  gameStart();
+  console.log("Green started")
+  green = true;
+  red = false;
+})
+squares[11].addEventListener("click", () => {
   gameStart();
   console.log("Green started")
   green = true;
@@ -34,6 +50,7 @@ function settingBoard() {
       squares[i].addEventListener('mousedown', function () { mouseIsDown = true })
       squares[i].addEventListener('mouseup', function () { mouseIsDown = false })
       squares[i].addEventListener('mousemove', function () {
+
         if (mouseIsDown) {
           onSquareClick(i)
           console.log("Hovering")
@@ -42,6 +59,7 @@ function settingBoard() {
     }
   }
 }
+//Changes the colour of boxs that are hovered over whislt mouse click is down 
 function onSquareClick(i) {
   if (green == true) {
     red = false;
@@ -57,22 +75,40 @@ function onSquareClick(i) {
     colSelected.innerHTML = "Red colour selected"
 
   }
-  WinCon();
+  WinCondition();
+
 
 }
+// Starts the game state
 function gameStart() {
   console.log("gamestarting");
   game = true;
   settingBoard()
 
 }
+// Win condition will go here
+function WinCondition() {
+  for (let i = 0; i < winCon.length; i++) {
+    var s1 = winCon[i][0];
+    var s2 = winCon[i][1];
+    var s3 = winCon[i][2];
+    var s4 = winCon[i][3];
+    var s5 = winCon[i][4];
+    var s6 = winCon[i][5];
+    var s7 = winCon[i][6];
+    var s8 = winCon[i][7];
+    var s9 = winCon[i][8];
 
-function WinCon() {
-  for (let i = 0; i < squares.length; i++) {
-
+    // needs fixing
+    if (squares[s1].getAttribute("class") == squares[s2].getAttribute("class") && squares[s2].getAttribute("class") == squares[s3].getAttribute("class")) {
+      if (squares[s1].getAttribute("class") == "0") {
+        alert("Red win condition");
+        resetBoard();
+      }
+    }
   }
 }
-
+//Button to reset the board 
 btnReset.addEventListener("click", () => {
   resetBoard();
 
@@ -82,6 +118,8 @@ function resetBoard() {
   for (let i = 0; i < squares.length; i++) {
     squares[parseInt(i)].setAttribute("class", "clear");
     squares[i].style.backgroundColor = "white";
+    colSelected.innerHTML = "";
+
     mouseIsDown = false;
     game = false;
   }
@@ -96,7 +134,6 @@ function tellPos(p) {
 
 }
 addEventListener('mousemove', tellPos, false)
-
 
 
 
